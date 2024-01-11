59959a42ae61af4ad659cf155a43719f4a59d1c0
mike wassermann <mikew194@protonmail.com>
2024-01-11 13:33:13 -0600

    added

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using CustomerApi.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+var connectionString = builder.Configuration.GetConnectionString("TESTDBV1Connection");
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddDbContext<DataBaseContext>(options => options.UseSqlServer(connectionString));
 
 var app = builder.Build();
 
